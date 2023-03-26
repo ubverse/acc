344973800a4fd80b105785425fa2b8936154953e
Caian Ertl <caian.ertl@mzgroup.com>
2023-03-26 04:16:36 -0300

    refactor: move secrets manager injector functionality to "@ubverse/smei"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { ConfigCollection } from './collection'
 export { AppConfigurationChecker } from './config'
-export { SecretsManagerEnvInjector } from './secret'
 export { ConfigValueType, ConfigValueMap, SupportedValueTypes, IConfig, IConfigOptions } from './types'
